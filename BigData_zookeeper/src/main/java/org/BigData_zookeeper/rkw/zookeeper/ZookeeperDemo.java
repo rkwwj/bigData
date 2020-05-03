7cac1116af85ade433a4543209536b281b148d0f
rkw <rkw@windows10.microdone.cn>
2020-05-03 09:53:36 +0800

    创建监听

--- BigData_zookeeper/src/main/java/org/BigData_zookeeper/rkw/zookeeper/ZookeeperDemo.java
+++ BigData_zookeeper/src/main/java/org/BigData_zookeeper/rkw/zookeeper/ZookeeperDemo.java
@@ -1,19 +1,47 @@
 package org.BigData_zookeeper.rkw.zookeeper;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.junit.Before;
+import org.junit.Test;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperDemo {
-
-	
-	
-	
-	public static void main(String[] args) throws IOException {
-		Watcher watcher = null;
-		ZooKeeper zk = new ZooKeeper("", 2000, watcher);
-		
-		
+	ZooKeeper zk = null;
+	@Before
+	public void inti() throws Exception{
+		 zk = new ZooKeeper("namenode001:2181,datanode001:2181,datanode002:2181", 2000,  new Watcher() {
+				@Override
+				public void process(WatchedEvent event) {
+					// TODO Auto-generated method stub
+					if (event.getType()==EventType.NodeDataChanged) {
+						System.out.println(event.getType());
+						System.out.println("节点发生变化");
+					}
+					try {
+						byte[] data = zk.getData("/aa",true,null);
+						try {
+							System.out.println(new String(data,"utf8"));
+						} catch (UnsupportedEncodingException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} catch (KeeperException | InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+	}
+	@Test
+	public void rkw()  throws IOException, KeeperException, InterruptedException {
+		byte[] data = zk.getData("/aa",true,null);
+		System.out.println(new String(data,"utf8"));
+		Thread.sleep(Long.MAX_VALUE);
 	}
 }
